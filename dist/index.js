f9f7580b97bfffbf1dc5ad64af9539979d007058
Kriegslustig <kriegslustig@kriegslustig.me>
2016-06-08 11:44:27 +0200

    Update the dist

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var Tg = require('tg-yarl');
+var notifier = require('node-notifier');
 var Rx = require('rx');
 
-module.exports = function telegram(config) {
-  var tg = Tg(config['telegram-api-token']);
-  return function (subject, message) {
-    return Rx.Observable.forkJoin(config['telegram-recipients'].map(function (r) {
-      return Rx.Observable.fromPromise(tg.sendMessage(r, subject + ' \n' + message));
-    }));
-  };
+module.exports = function desktop(config) {
+  var notify = Rx.Observable.fromNodeCallback(notifier.notify.bind(notifier));
+  return function (title, text) {
+    return notify({
+      title: title, text: text, open: text
+    }).takeUntilWithTime(1000);
+  }; // Time out gracefully if nothing happens
 };
